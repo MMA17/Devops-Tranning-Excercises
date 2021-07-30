66af7286133d8adcc1e38230261f63a660794d23
viet <nguyenhoangviet170500@gmail.com>
2021-07-30 20:54:52 +0700

    update DOTRAIN 83

--- DOTRAIN 83/main.py
+++ DOTRAIN 83/main.py
@@ -6,16 +6,19 @@ url = 'https://bizflycloud.vn/tin-tuc/caas-20210622161033206.htm'
 test = requests.get(url)
 soup = BeautifulSoup(test.text, 'html.parser')
 
-wr.write("name/title, content, created date, url\n")
-wr.write(str(soup.title.string) + ", ")
+wr.write("name/title; content; created date; url\n")
+wr.write(str(soup.title.string) + "; ")
 
 # print (soup.get_text('detail-content'))
 # for data in soup.find_all('div', {'class':'detail-content'}):
 #     print(data.text)
 for content in soup.find_all('p'):
-    wr.write(content.text+", ")
+    a = content.text
+    a = a.replace(';',".")
+    wr.write(a)
+wr.write(";")
 
 date = soup.find('div', {'class': 'read-time'})
-wr.write(date.text+", ")
+wr.write(date.text+"; ")
 
 wr.write(url)

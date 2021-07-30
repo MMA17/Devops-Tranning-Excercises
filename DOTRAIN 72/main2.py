0497ba35331f59ae196fda2cbf489235b523cd84
viet <nguyenhoangviet170500@gmail.com>
2021-07-30 10:47:43 +0700

    Revert "DOTAIN 84"
    This reverts commit 587ad3da673e87a83082718b8b47b730938eed33.

--- DOTRAIN 72/main2.py
+++ DOTRAIN 72/main2.py
@@ -1,6 +1,5 @@
 import random
 
-
 class bankAccount:
     balance = 0
 
@@ -9,40 +8,28 @@ class bankAccount:
 
     def deposit(self, amount):
         self.balance += amount
-        print("Nap so tien "
-              + str(amount)
-              + " Tai khoan co: "
-              + str(self.balance))
-
+        print("Nap so tien " + str(amount) + " Tai khoan co: " + str(self.balance))
 
 class VPBank(bankAccount):
     def withdraw(self, amount):
         self.balance = self.balance - amount - amount*0.1
-        print("VPB Rut so tien " + str(amount)
-              + " Tai khoan co: " + str(self.balance))
-
+        print("VPB Rut so tien " + str(amount) + " Tai khoan co: " + str(self.balance))
 
 class TechcomBank(bankAccount):
     def withdraw(self, amount):
         self.balance = self.balance - amount - amount*0.1
-        print("TCB Rut so tien " + str(amount)
-              + " Tai khoan co: " + str(self.balance))
+        print("TCB Rut so tien " + str(amount) + " Tai khoan co: " + str(self.balance))
 
-# VPBank(12000).withdraw(1000)
-
+#VPBank(12000).withdraw(1000)
 
 def run():
-    list = [
-        (VPBank(10000), 3200),
-        (TechcomBank(23000), 9555),
-        (TechcomBank(50000), 33000)]
+    list  = [(VPBank(10000), 3200), (TechcomBank(23000), 9555), (TechcomBank(50000), 33000)]
     for x in list:
-        temp = random.randint(0, 1)
-        # print (temp)
+        temp = random.randint(0,1)
+        #print (temp)
         if temp == 1:
             x[0].deposit(int(x[1]))
         else:
             x[0].withdraw(int(x[1]))
-
 
 run()

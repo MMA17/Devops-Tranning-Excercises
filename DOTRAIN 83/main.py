8a03e25461ffd46cfbd61b62b0fb9b293b6819b6
viet <nguyenhoangviet170500@gmail.com>
2021-07-30 20:26:52 +0700

    DOTRAIN 83

---
+++ DOTRAIN 83/main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+wr = open('output.csv', 'w')
+url = 'https://bizflycloud.vn/tin-tuc/caas-20210622161033206.htm'
+test = requests.get(url)
+soup = BeautifulSoup(test.text, 'html.parser')
+
+wr.write("name/title, content, created date, url\n")
+wr.write(str(soup.title.string) + ", ")
+
+# print (soup.get_text('detail-content'))
+# for data in soup.find_all('div', {'class':'detail-content'}):
+#     print(data.text)
+for content in soup.find_all('p'):
+    wr.write(content.text+", ")
+
+date = soup.find('div', {'class': 'read-time'})
+wr.write(date.text+", ")
+
+wr.write(url)
